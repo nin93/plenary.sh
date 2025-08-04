8693903a1eaaf255898a25a3dd9251ff045f3406
Elia Franzella <franzella.elia@gmail.com>
2025-08-04 17:15:30 +0200

    refactor: Enhance gentoo's `upkg`

--- src/distro/gentoo/upkg.sh
+++ src/distro/gentoo/upkg.sh
@@ -1,51 +1,113 @@
 #!/bin/env -S bash
 
+PROGRAM="upkg"
+
+OPT_SYNC=""
+OPT_UPDATE=""
+OPT_CLEAN=""
+OPT_ASK=""
+
+function usage {
+  cat << EOF
+Usage: $PROGRAM [OPTIONS] [PORTAGE_SET]
+
+Opinionated gentoo oneliner for system updating.
+
+OPTIONS
+  -s, --sync          sync portage tree with upstream for all enabled repositories
+  -u, --update        trigger PORTAGE_SET update.
+  -c, --clean         remove old packages, binaries, sources
+  -a, --ask           ask before operations
+  -h, --help          print this help message
+
+Argument PORTAGE_SET is any supported portage set. Defaults to @world.
+EOF
+}
+
+function die {
+  echo "$*" >&2
+  usage >&2
+  exit 1
+} 
+
 function sync {
   emerge --sync
 }
 
 function update {
-  emerge \
-    --verbose --quiet --deep --newuse --changed-use --update --getbinpkg --with-bdeps=y \
-    --autounmask-write --autounmask-use=y --autounmask-license=y --keep-going \
-    @world
+  local emerge_opts=(
+    "--verbose" "--quiet" "--deep" "--newuse" "--changed-use" "--update" "--getbinpkg" "--with-bdeps=y"
+    "--autounmask-write" "--autounmask-use=y" "--autounmask-license=y" "--keep-going"
+  )
+
+  if [[ $OPT_ASK == "y" ]]; then
+    emerge_opts+=("--ask");
+  fi
+
+  emerge "${emerge_opts[@]}" "$1"
 }
 
 function clean {
-  emerge --depclean --quiet && eclean-dist && eclean-pkg
+  local emerge_opts=("--depclean" "--quiet")
+
+  if [[ $OPT_ASK == "y" ]]; then
+    emerge_opts+=("--ask");
+  fi
+
+  emerge "${emerge_opts[@]}" && eclean-dist && eclean-pkg
 }
 
 function main {
-  while getopts ":usc" opt; do
-    case $opt in
-      s)
-        SYNC=true
-        ;;
-      u)
-        UPDATE=true
-        ;;
-      c)
-        CLEAN=true
-        ;;
-      \?)
-        echo "Invalid option: -$OPTARG" >&2
-        ;;
+  if ! options=$(getopt -o "hsuca" -l "help,sync,update,clean,ask" -- "$@"); then
+    die
+  fi
+
+  set -- $options
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+        usage
+        exit 0;;
+
+      -s|--sync)
+        OPT_SYNC=y
+        shift;;
+
+      -u|--update)
+        OPT_UPDATE=y
+        shift;;
+
+      -c|--clean)
+        OPT_CLEAN=y
+        shift;;
+
+      -a|--ask)
+        OPT_ASK=y
+        shift;;
+
+      --)
+        shift
+        break;;
+
+      -*)
+        die "$PROGRAM: error - unrecognized option $1";;
     esac
   done
 
-  if [[ -n "$SYNC" ]]; then
+  if [[ $OPT_SYNC == "y" ]]; then
     sync || exit 1
   fi
 
-  if [[ -n "$UPDATE" ]]; then
-    update || exit 1
+  if [[ $OPT_UPDATE == "y" ]]; then
+    update "${1:-@world}" || exit 1
   fi
 
-  if [[ -n "$CLEAN" ]]; then
+  if [[ $OPT_CLEAN == "y" ]]; then
     clean || exit 1
   fi
 
   exit 0
 }
 
-main $@
+main "$@"
