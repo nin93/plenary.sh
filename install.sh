8f0bbfbdb4cfc086084c4773da4df86c6e29658b
Elia Franzella <franzella.elia@gmail.com>
2025-08-11 17:58:37 +0200

    fix: Use "$HOME/bin" as default installation dir

--- install.sh
+++ install.sh
@@ -1,61 +1,73 @@
 #!/bin/env -S bash
 
 function usage {
-	cat << EOF
-Usage: $PROGRAM [...OPTIONS]
+	cat <<-EOF
+		Usage: $PROGRAM [...OPTIONS]
 
-Scripts installer.
+		Scripts installer.
 
-OPTIONS
-  -b, --binpath          set the installation path for scripts, default: /usr/local/bin
-      --keep-extension   keep .sh extension for installed scripts
-  -h, --help             print this help message
-EOF
+		OPTIONS
+		  -b, --binpath          set the installation path for scripts, default: ~/bin
+		      --keep-extension   keep .sh extension for installed scripts
+		  -h, --help             print this help message
+	EOF
 }
 
 function die {
-  echo "$*" >&2;
-  usage >&2
+	echo "$*" >&2
+	usage >&2
 
-  exit 1;
-} 
+	exit 1
+}
 
 function main {
-  if ! options=$(getopt -o "b:" -l "binpath:,keep-extension" -- "$@"); then
-    die
-  fi
+	if ! options=$(getopt -o "b:" -l "binpath:,keep-extension" -- "$@"); then
+		die
+	fi
 
-  eval set -- $options
+	eval set -- $options
 
-  while [ $# -gt 0 ]; do
-    case $1 in
-      -b|--binpath)
-        local binpath=$2; shift 2;;
-      --keep-extension)
-        local keep_extension=y; shift 2;;
-      --)
-        shift; break;;
-      -*)
-        die "$0: error - unrecognized option $1";;
-      *)
-        break;;
-    esac
-  done
-  
-  binpath="${binpath:-/usr/local/bin/}"
+	while [ $# -gt 0 ]; do
+		case $1 in
+		-b | --binpath)
+			local binpath=$2
+			shift 2
+			;;
 
-  find "$(dirname "$0")/src" -type f -name '*.sh' -print0 | while read -d $'\0' filename; do
-    local out_filename="${filename##*/}";
+		--keep-extension)
+			local keep_extension=y
+			shift 2
+			;;
 
-    if ! [[ $keep_extension == 'y' ]]; then
-      out_filename="${out_filename%.sh}"
-    fi
+		--)
+			shift
+			break
+			;;
 
-    # Respect `~` expansion
-    eval fullpath=$(echo "$binpath/$out_filename")
+		-*)
+			die "$0: error - unrecognized option $1"
+			;;
 
-    install "$filename" "$fullpath" -m 755
-  done
+		*)
+			break
+			;;
+		esac
+	done
+
+	binpath="${binpath:-${HOME}/bin/}"
+
+	find "$(dirname "$0")/src" -type f -name '*.sh' -print0 | while read -d $'\0' filename; do
+		local out_filename="${filename##*/}"
+
+		if ! [[ $keep_extension == 'y' ]]; then
+			out_filename="${out_filename%.sh}"
+		fi
+
+		# Respect `~` expansion
+		eval fullpath=$(echo "$binpath/$out_filename")
+
+		install "$filename" "$fullpath" -m 755
+	done
 }
 
 main "$@"
